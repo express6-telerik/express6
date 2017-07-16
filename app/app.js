ba577885e9f9193138849b2c253066cfcd50cb47
LammaSF <lilibabacheva@yahoo.com>
2017-07-16 23:19:02 +0300

    errorpage

--- app/app.js
+++ app/app.js
@@ -1,4 +1,4 @@
-/*globals __dirname*/
+/* globals __dirname*/
 
 const express = require('express');
 const favicon = require('serve-favicon');
@@ -8,19 +8,18 @@ const path = require('path');
 const app = express();
 app.listen(3030, () => console.log('Magic'));
 
-//const init = () => {
+// const init = () => {
     app.set('view engine', 'pug');
     app.use(favicon(path.join(__dirname, '../public', '/imgs/fav.ico')));
 
     app.get('/404', (req, res) => {
-
-        res.send('<h1>Error page</h1>');
+        return res.render('errorpage');
     });
     app.get('/home', (req, res) => {
         return res.render('home');
     });
-    //return Promise.resolve(app);
-//};
+    // return Promise.resolve(app);
+// };
 
 
 module.exports = app;
