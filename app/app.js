f2d0e64d0b8904d4ed6a2717feb22d7d0e6d3427
LammaSF <lilibabacheva@yahoo.com>
2017-07-17 03:08:47 +0300

    navigation + css

--- app/app.js
+++ app/app.js
@@ -4,13 +4,16 @@ const express = require('express');
 const favicon = require('serve-favicon');
 const path = require('path');
 
-
 const app = express();
-app.listen(3030, () => console.log('Magic'));
 
-// const init = () => {
-    app.set('view engine', 'pug');
-    app.use(favicon(path.join(__dirname, '../public', '/imgs/fav.ico')));
+    app.listen(3030, () => console.log('Magic'));
+
+    // const init = () => {
+        app.set('view engine', 'pug');
+        app.use(favicon(path.join(__dirname, '../public', '/imgs/fav.ico')));
+    app.use('/static',
+        express.static(
+            path.join(__dirname, '../public')));
 
     app.get('/404', (req, res) => {
         return res.render('errorpage');
