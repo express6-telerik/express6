98c0cd458084ad9046b721a7d30b7c1a9d621c02
LammaSF <lilibabacheva@yahoo.com>
2017-07-30 01:38:07 +0300

    eslint delete unneeded files

--- config/config.js
+++ config/config.js
@@ -2,4 +2,4 @@ const port = 3080;
 const connectionString = 'mongodb://localhost:27017/room-db';
 const sessionSecret = 'secret';
 
-module.exports = { port, connectionString, sessionSecret};
+module.exports = { port, connectionString, sessionSecret };
