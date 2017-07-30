d9f3c74fc007e58ec06be75fb44c9f72a6be1b62
LammaSF <lilibabacheva@yahoo.com>
2017-07-31 02:42:41 +0300

    chat works

--- app/app.js
+++ app/app.js
@@ -5,18 +5,24 @@ const favicon = require('serve-favicon');
 const path = require('path');
 
 
-
 const init = (data) => {
    // const http = require('http');
     const app = express();
-    const http = require('http').createServer(app);
-    const io = require('socket.io')(http);
+  
+
+    const server = require('http').createServer(app);
+    const io = require('socket.io')(server);
+
+
+    app.get('/chat', (req, res) => {
+    res.sendFile(path.join(__dirname, './chat.html'));
+    });
+
+app.use(favicon(path.join(__dirname, '../public', '/imgs/fav.ico')));
   //  const io = require('socket.io')(http);
     //  config
     require('./config').init(app, data);
     require('./auth').init(app, data);
-    app.use(favicon(path.join(__dirname, '../public', '/imgs/fav.ico')));
-
     app.use('/public', express.static('static'));
 
     // global var
@@ -32,19 +38,6 @@ const init = (data) => {
     app.use('/libs',
         express.static(
             path.join(__dirname, '../node_modules')));
-
-    io.sockets.on('connection', function(client){
-        client.on('message', function(msg){
-            client.emit('message', msg);
-        });
-    });
-
-    // io.on('connection', function(socket) {
-    //     socket.on('chat message', function(msg) {
-    //         io.emit('chat message', msg);
-    //     });
-    // });
-
 
     require('./routers').init(app, data);
 
--- server.js
+++ server.js
@@ -3,14 +3,22 @@ const async = () => {
 };
 
 const config = require('./config');
+const socket = require('socket.io');
 
 async()
     .then(() => require('./db').init(config.connectionString))
     .then((db) => require('./data').init(db))
     .then((data) => require('./app').init(data))
     .then((app) => {
-        app.listen(config.port, () =>
-            console.log(`Listen port: ${config.port}`));
+       const server = app.listen(config.port, () =>
+             console.log(`Listen port: ${config.port}`));
+        const io = socket(server);
+        io.on('connection', (websocket) => {
+            console.log('connection: ', websocket.id);
+            websocket.on('msg', (msg) => {
+                console.log('received msg: ', msg);
+            });
+        });
     })
     .catch((err) => {
         console.log(err);
